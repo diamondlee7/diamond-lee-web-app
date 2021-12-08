8b00243ec0d58aeed726daf51b141b4d693e3257
Diamond Lee <diamond.f.lee7@gmail.com>
2021-12-07 20:10:55 -0800

    looks nice want to save it before I ruin the styling

--- diamond-lee-web-app/src/app/app.module.ts
+++ diamond-lee-web-app/src/app/app.module.ts
@@ -5,23 +5,31 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatDividerModule } from '@angular/material/divider';
-import { HeaderComponent } from './header/header.component'
+import { MatDividerModule } from '@angular/material/divider';
+import { HeaderComponent } from './header/header.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list'
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    HeaderComponent
+    HeaderComponent,
+    SideNavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatDividerModule
+    MatDividerModule,
+    MatIconModule,
+    MatListModule,
+    MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { 
+export class AppModule {
 }
