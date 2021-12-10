2a6d28dc8190b3f6a7bdc0688e4eb10b494b8fd7
Diamond Lee <diamond.f.lee7@gmail.com>
2021-12-09 22:59:32 -0800

    index.html

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + './dist/diamond-lee-web-app'));
+app.use(express.static('index.html' + './dist/diamond-lee-web-app'));
 
 app.get('/*', function(req, res) {
 
-    res.sendFile(path.join(__dirname + '/dist/diamond-lee-web-app/index.html'));
+    res.sendFile(path.join('index.html' + '/dist/diamond-lee-web-app/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
