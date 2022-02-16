69d9d0a1aee8117fdba75afc3b06071d0949461c
Diamond Lee <diamond.f.lee7@gmail.com>
2022-02-16 15:59:53 -0800

    try

--- src/server.js
+++ src/server.js
@@ -16,6 +16,6 @@ app.use(express.static('index.html' + '../dist/diamond-lee-web-app'));
 
 // Start the app by listening on the default Heroku port
 app.get('*', function(req, res) {
-    res.sendFile('../dist/diamond-lee-web-app/index.html');
+    res.sendFile('app/dist/diamond-lee-web-app/index.html');
 });
 app.listen(process.env.PORT || 8080);
