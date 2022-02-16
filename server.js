6e7799171a9c580e0011f4a511f1c7784411fb01
Diamond Lee <diamond.f.lee7@gmail.com>
2022-02-16 14:17:06 -0800

    update dependencies and use proper route in server file

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
+function requireHTTPS(req, res, next) {
+    // The 'x-forwarded-proto' check is for Heroku
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+        return res.redirect('https://' + req.get('host') + req.url);
+    }
+    next();
+}
+
 //Install express server
 const express = require('express');
-const path = require('path');
-
 const app = express();
+const app = express();
+app.use(requireHTTPS);
 
 // Serve only the static files form the dist directory
-app.use(express.static('index.html' + '/dist/diamond-lee-web-app'));
-
-app.get('/*', function(req, res) {
-
-    res.sendFile('dist/diamond-lee-web-app/index.html');
-});
+app.use(express.static('index.html' + './dist/diamond-lee-web-app'));
 
 // Start the app by listening on the default Heroku port
+app.get('/*', function(req, res) {
+    res.sendFile('./dist/diamond-lee-web-app/index.html');
+});
+
 app.listen(process.env.PORT || 8080);
