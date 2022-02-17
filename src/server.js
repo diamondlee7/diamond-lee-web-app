0f072fe4ff1c2f32a1cceb7d5a141b197af30917
Diamond Lee <diamond.f.lee7@gmail.com>
2022-02-16 17:03:42 -0800

    syntax

--- src/server.js
+++ src/server.js
@@ -13,10 +13,10 @@ const app = express();
 app.use(requireHTTPS);
 
 // Serve only the static files form the dist directory
-app.use(express.static('index.html' + 'src'));
+app.use(express.static('index.html', { root: path.join(__dirname, '../src') }));
 
 // Start the app by listening on the default Heroku port
 app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'src', 'index.html'));
+    res.sendFile('index.html', { root: path.join(__dirname, '../src') });
 });
 app.listen(process.env.PORT || 8080);
