303f40d7a5049f56cd0607441815d470abb645ba
Diamond Lee <diamond.f.lee7@gmail.com>
2022-03-27 14:46:45 -0700

    sendFile

--- src/server.js
+++ src/server.js
@@ -19,7 +19,7 @@ app.use(express.static(__dirname + '/src/index.html'));
 const fs = require('fs');
 
 app.get('/', function(req, res) {
-    res.send(browserRefresh('index.html'));
+    res.sendFile(browserRefresh('index.html'));
 });
 
 function browserRefresh(filePath) {
